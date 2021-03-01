3b573c9a2129e83e47efaa93890357239359ac3d
ErfanhanifezadeDeveloper <erfanhanifezade@gmail.com>
2021-03-01 22:38:40 +0330

    just for test

--- app.js
+++ app.js
@@ -2,19 +2,9 @@ const request = require("postman-request");
 
 const url = "https://api.openweathermap.org/data/2.5/weather?lat=37.09024&lon=-95.712891&appid=6cead677b4563809592af602e1e2344a";
 
-// request({url: url} , (err , res)=>{
-//     const toJS = JSON.parse(res.body);
+request({url: url} , (err , res)=>{
+    const toJS = JSON.parse(res.body);
 
-//     // console.log(toJS);  
-// });
-
-
-const obj = {name: "erfan"};
-Object.defineProperty(obj , "name" , {
-    value: "erfann",
-    writable: false
+    console.log(toJS);  
 });
 
-obj.name = "test";
-
-console.log(obj);
