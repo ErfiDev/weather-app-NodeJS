b89b86d66d410c3032634d24a01f59ecb1f115bd
ErfanhanifezadeDeveloper <erfanhanifezade@gmail.com>
2021-03-03 22:20:23 +0330

    add yargs package

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const chalk = require("chalk");
 
 const url = "https://api.openweathermap.org/data/2.5/weather?lat=37.09024&lon=-95.712891&appid=6cead677b4563809592af602e1e2344a&units=metric";
 
+const mapBox = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZXJmYW5oYW5pZmV6YWRlMTEiLCJhIjoiY2trY2N5YnU2MDg2dzJwcGl3MHQ4dmd1ayJ9.6Watree8ZLL8rLI8HO29cg";
+
 request({url: url} , (err , res)=>{
     if(err)
     {
@@ -23,4 +25,7 @@ request({url: url} , (err , res)=>{
     }
 });
 
+request({url: mapBox} , (err , res)=>{
+    console.log(JSON.parse(res.body));
+});
 
