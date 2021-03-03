4842e05608b248b72e37ac016a373e366bc6b635
ErfanhanifezadeDeveloper <erfanhanifezade@gmail.com>
2021-03-03 16:38:40 +0330

    error handling

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const request = require("postman-request");
 
-const url = "https://api.openweathermap.org/data/2.5/weather?lat=37.09024&lon=-95.712891&appid=6cead677b4563809592af602e1e2344a";
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=37.09024&lon=-95.712891&appid=6cead677b4563809592af602e1e2344a&units=metric";
 
 request({url: url} , (err , res)=>{
-    const toJS = JSON.parse(res.body);
+    if(err)
+    {
+        console.log(err);
+    }else{
+        const toJS = JSON.parse(res.body);
 
-    console.log(toJS);  
+        console.log(toJS);  
+    }
 });
+
 
