ce260cf8c0e35195301459815618d35837704602
ErfanhanifezadeDeveloper <erfanhanifezade@gmail.com>
2021-03-03 18:45:13 +0330

    error handling finish

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const request = require("postman-request");
+const chalk = require("chalk");
 
 const url = "https://api.openweathermap.org/data/2.5/weather?lat=37.09024&lon=-95.712891&appid=6cead677b4563809592af602e1e2344a&units=metric";
 
 request({url: url} , (err , res)=>{
     if(err)
     {
-        console.log(err);
-    }else{
-        const toJS = JSON.parse(res.body);
-
-        console.log(toJS);  
+        console.log("Unable to connect to weather service!");
+    }
+    else
+    {
+        const response = JSON.parse(res.body);
+        if(response.message)
+        {
+            console.log(chalk.red(response.message));
+        }
+        else
+        {
+            console.log("City Name: " + chalk.blue(response.name));
+            console.log("Temp: " + chalk.green(`${response.main.temp}C`));
+        }
     }
 });
 
