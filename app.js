f39dfec2e3f73812208862a541f074862d34c876
ErfanhanifezadeDeveloper <erfanhanifezade@gmail.com>
2021-03-09 20:50:58 +0330

    Send city name and get weather

--- app.js
+++ app.js
@@ -1,31 +1,46 @@
 const request = require("postman-request");
 const chalk = require("chalk");
 
-const url = "https://api.openweathermap.org/data/2.5/weather?lat=37.09024&lon=-95.712891&appid=6cead677b4563809592af602e1e2344a&units=metric";
+function getGeocode(location)
+{
+    const urlMapBox = `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}
+    .json?access_token=pk.eyJ1IjoiZXJmYW5oYW5pZmV6YWRlMTEiLCJhIjoiY2trY2N5YnU2MDg2dzJwcGl3MHQ4dmd1ayJ9.6Watree8ZLL8rLI8HO29cg`;
 
-const mapBox = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZXJmYW5oYW5pZmV6YWRlMTEiLCJhIjoiY2trY2N5YnU2MDg2dzJwcGl3MHQ4dmd1ayJ9.6Watree8ZLL8rLI8HO29cg";
+    request({ url: urlMapBox } , (err , res)=>{
+        if(err){ console.log(chalk.red.bold(err.code)); }
 
-request({url: url} , (err , res)=>{
-    if(err)
-    {
-        console.log("Unable to connect to weather service!");
-    }
-    else
-    {
-        const response = JSON.parse(res.body);
-        if(response.message)
+        else{
+            const response = JSON.parse(res.body);
+            const {features} = response;
+            const longi = features[0].center[0];
+            const lati = features[0].center[1];
+            getWeather(longi , lati);
+        }
+    });
+}
+
+function getWeather(longi , lati)
+{
+    const urlWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lati}&lon=${longi}&appid=6cead677b4563809592af602e1e2344a&units=metric`
+    request({url: urlWeather} , (err , res)=>{
+        if(err)
         {
-            console.log(chalk.red(response.message));
+            console.log(err);
         }
         else
         {
-            console.log("City Name: " + chalk.blue(response.name));
-            console.log("Temp: " + chalk.green(`${response.main.temp}C`));
+            const response = JSON.parse(res.body);
+            if(response.message)
+            {
+                console.log(chalk.red(response.message));
+            }
+            else
+            {
+                console.log("City Name: " + chalk.blue(response.name));
+                console.log("Temp: " + chalk.green(`${response.main.temp}C`));
+            }
         }
-    }
-});
+    });
+}
 
-request({url: mapBox} , (err , res)=>{
-    console.log(JSON.parse(res.body));
-});
-
+getGeocode("karaj");
