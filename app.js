ca9938b96f41d1f54f319425851eab50d3e2f2b9
ErfanhanifezadeDeveloper <erfanhanifezade@gmail.com>
2021-03-10 22:30:09 +0330

    using yargs

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const chalk = require("chalk");
+const yargs = require("yargs");
 const getGeocode = require("./utils/geocode");
 const getWeather = require("./utils/getWeather");
 
-
-getGeocode("tehran" , (err , res)=>{
-    if(err) { console.log(chalk.red.bold(err)) }
-    else{
-        getWeather(res[0] , res[1]);
+yargs.command({
+    command: "city",
+    describe: "enter your city name",
+    builder: {
+      name: {
+        describe: "your city name",
+        demandOption: true,
+        type: "string",
+      }
+    },
+    handler(argv){
+        getGeocode(argv.name , (err , res)=>{
+            if(err) { console.log(chalk.red.bold(err)) }
+            else{
+                getWeather(res[0] , res[1]);
+            }
+        });
     }
 });
+
+console.log(yargs.argv);
