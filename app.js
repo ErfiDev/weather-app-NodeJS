eb3436699ca9d019d1d906cc23739b8a180b544c
ErfanhanifezadeDeveloper <erfanhanifezade@gmail.com>
2021-03-13 16:55:38 +0330

    refactoring app.js

--- app.js
+++ app.js
@@ -1,26 +1,15 @@
 const chalk = require("chalk");
-const yargs = require("yargs");
 const getGeocode = require("./utils/geocode");
 const getWeather = require("./utils/getWeather");
 
-yargs.command({
-    command: "city",
-    describe: "enter your city name",
-    builder: {
-      name: {
-        describe: "your city name",
-        demandOption: true,
-        type: "string",
-      }
-    },
-    handler(argv){
-        getGeocode(argv.name , (err , res)=>{
-            if(err) { console.log(chalk.red.bold(err)) }
-            else{
-                getWeather(res[0] , res[1]);
-            }
-        });
-    }
-});
+const address = process.argv[2];
 
-console.log(yargs.argv);
+if(!address){ console.log(chalk.blue("please provide address")) }
+else{
+  getGeocode(address , (err , res)=>{
+      if(err) { console.log(chalk.red.bold(err)) }
+      else{
+          getWeather(res[0] , res[1]);
+      }
+  });
+}
